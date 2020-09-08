fa60d2264ef34b4c5f28de5b86f2828524619c95
Gideon Chukwu <chuksgid@gmail.com>
2020-09-08 23:51:47 +0100

    Configuring project with MDC

---
+++ app/src/main/java/com/octacoresoftwares/mito/MikoApplication.kt
@@ -0,0 +1,16 @@
+package com.octacoresoftwares.mito
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+
+class MikoApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initTheme()
+    }
+
+    private fun initTheme() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+}
